0cf6d5a6ecc1b6f1dff7a488f4b87f5085084a4f
Luiz Nascimento <luizguilhermesn@gmail.com>
2025-05-23 22:02:48 -0300

    chore: refactor testing

--- 2048_AI/agent.py
+++ 2048_AI/agent.py
@@ -1,83 +1,199 @@
-import torch
-import random
 import numpy as np
-from collections import deque
-from game import Game2048, Direction
-from model import DQN, QTrainer
-from typing import List, Tuple, Deque
+from game import Game2048
+from typing import List, Tuple
+import concurrent.futures
 
-class Agent:
-    def __init__(self) -> None:
-        self.n_games: int = 0
-        self.epsilon: float = 1.0  # Randomness - start with 100% exploration
-        self.epsilon_min: float = 0.01  # Minimum exploration rate
-        self.epsilon_decay: float = 0.999  # Decay rate for exploration
-        self.gamma: float = 0.9  # Discount rate
-        self.memory: Deque = deque(maxlen=100_000)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = DQN(16, 256, 4)  # Input size: 4x4 board, Output size: 4 directions
-        self.model = self.model.to(self.device)
-        self.trainer = QTrainer(self.model, lr=0.001, gamma=self.gamma)
+NUM_DIRECTIONS = 6  # Novo jogo tem 6 direções
+GRID_SIZE = 5
 
-    def get_state(self, game: Game2048) -> np.ndarray:
-        return game.get_state()
+# --- Constantes e helpers do game.py ---
+GRID_COORDENATES = [(i, j) for i, cols in enumerate([range(0, 3), range(0, 4), range(0, 5), range(0, 4), range(0, 3)]) for j in cols]
+MAIN_DIAGONAL = [
+    [(0, 2), (1, 3), (2, 4)],
+    [(0, 1), (1, 2), (2, 3), (3, 3)],
+    [(0, 0), (1, 1), (2, 2), (3, 2), (4, 2)],
+    [(1, 0), (2, 1), (3, 1), (4, 1)],
+    [(2, 0), (3, 0), (4, 0)]
+]
+SECONDARY_DIAGONAL = [
+    [(0, 0), (1, 0), (2, 0)],
+    [(0, 1), (1, 1), (2, 1), (3, 0)],
+    [(0, 2), (1, 2), (2, 2), (3, 1), (4, 0)],
+    [(1, 3), (2, 3), (3, 2), (4, 1)],
+    [(2, 4), (3, 3), (4, 2)]
+]
 
-    def remember(self, state: np.ndarray, action: List[float], reward: float, 
-                next_state: np.ndarray, done: bool) -> None:
-        self.memory.append((state, action, reward, next_state, done))
 
-    def train_long_memory(self) -> None:
-        if len(self.memory) > 100:
-            mini_sample = random.sample(self.memory, 100)
+def _merge(line: list[int]) -> tuple[list[int], int]:
+    non_zeros = [x for x in line if x != 0]
+    line = non_zeros + [0] * (len(line) - len(non_zeros))
+    score = 0
+    for i in range(len(line) - 1):
+        if line[i] != 0 and line[i] == line[i + 1]:
+            line[i] *= 2
+            score += line[i]
+            line[i + 1] = 0
+    non_zeros = [x for x in line if x != 0]
+    line = non_zeros + [0] * (len(line) - len(non_zeros))
+    return line, score
+
+
+def move_board(board: list[list[int]], direction: int) -> tuple[list[list[int]], int, bool]:
+    """
+    Realiza o movimento na matriz board na direção especificada, retornando a nova matriz, score ganho e se houve movimento.
+    """
+    import copy
+    new_board = copy.deepcopy(board)
+    moved = False
+    score = 0
+    if direction == 0:  # main diagonal up
+        for k in range(len(new_board)):
+            coord = MAIN_DIAGONAL[k]
+            transversal = [new_board[i][j] for (i, j) in coord]
+            new_transversal, add_score = _merge(transversal)
+            for l in range(len(transversal)):
+                if transversal[l] != new_transversal[l]:
+                    moved = True
+                    i, j = coord[l]
+                    new_board[i][j] = new_transversal[l]
+            score += add_score
+    elif direction == 1:  # secondary diagonal up
+        for k in range(len(new_board)):
+            coord = SECONDARY_DIAGONAL[k]
+            transversal = [new_board[i][j] for (i, j) in coord]
+            new_transversal, add_score = _merge(transversal)
+            for l in range(len(transversal)):
+                if transversal[l] != new_transversal[l]:
+                    moved = True
+                    i, j = coord[l]
+                    new_board[i][j] = new_transversal[l]
+            score += add_score
+    elif direction == 2:  # right
+        for k in range(len(new_board)):
+            row = [new_board[i][j] for (i, j) in GRID_COORDENATES if i == k]
+            row.reverse()
+            new_row, add_score = _merge(row)
+            new_row.reverse()
+            for j in range(len(row)):
+                if new_board[k][j] != new_row[j]:
+                    moved = True
+                    new_board[k][j] = new_row[j]
+            score += add_score
+    elif direction == 3:  # main diagonal down
+        for k in range(len(new_board)):
+            coord = MAIN_DIAGONAL[k]
+            transversal = [new_board[i][j] for (i, j) in coord]
+            transversal.reverse()
+            new_transversal, add_score = _merge(transversal)
+            new_transversal.reverse()
+            for l in range(len(transversal)):
+                i, j = coord[l]
+                if new_board[i][j] != new_transversal[l]:
+                    moved = True
+                    new_board[i][j] = new_transversal[l]
+            score += add_score
+    elif direction == 4:  # secondary diagonal down
+        for k in range(len(new_board)):
+            coord = SECONDARY_DIAGONAL[k]
+            transversal = [new_board[i][j] for (i, j) in coord]
+            transversal.reverse()
+            new_transversal, add_score = _merge(transversal)
+            new_transversal.reverse()
+            for l in range(len(transversal)):
+                i, j = coord[l]
+                if new_board[i][j] != new_transversal[l]:
+                    moved = True
+                    new_board[i][j] = new_transversal[l]
+            score += add_score
+    elif direction == 5:  # left
+        for k in range(len(new_board)):
+            row = [new_board[i][j] for (i, j) in GRID_COORDENATES if i == k]
+            new_row, add_score = _merge(row)
+            for j in range(len(row)):
+                if new_board[k][j] != new_row[j]:
+                    moved = True
+                    new_board[k][j] = new_row[j]
+            score += add_score
+    return new_board, score, moved
+
+def is_valid_move_board(board: list[list[int]], direction: int) -> bool:
+    _, _, moved = move_board(board, direction)
+    return moved
+
+def score_move_for_expectimax(args: tuple[list[list[int]], int, int]) -> tuple[int, float]:
+    board, move, max_depth = args
+    new_board, _, moved = move_board(board, move)
+    if moved:
+        agent = ExpectimaxAgent(max_depth)
+        score = agent.expectimax(new_board, max_depth - 1, False)
+        return (move, score)
+    else:
+        return (move, float('-inf'))
+
+class ExpectimaxAgent:
+    def __init__(self, max_depth: int = 3) -> None:
+        self.max_depth: int = max_depth
+
+    def get_state(self, game: Game2048) -> List[List[int]]:
+        return [row[:] for row in game.grid]
+
+    def get_action(self, state: List[List[int]], game: Game2048 = None) -> List[float]:
+        assert game is not None, 'ExpectimaxAgent precisa de uma instância de Game2048.'
+        board = [row[:] for row in game.grid]
+        best_move = self.expectimax_decision(board)
+        final_move = [0] * NUM_DIRECTIONS
+        if best_move is not None:
+            final_move[best_move] = 1
+        return final_move
+
+    def expectimax_decision(self, board: List[List[int]]) -> int:
+        args = [(board, move, self.max_depth) for move in range(NUM_DIRECTIONS)]
+        with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_DIRECTIONS) as executor:
+            results = list(executor.map(score_move_for_expectimax, args))
+        best_move, best_score = max(results, key=lambda x: x[1])
+        return best_move
+
+    def expectimax(self, board: List[List[int]], depth: int, is_player_turn: bool) -> float:
+        if depth == 0 or self.is_game_over(board):
+            return self.evaluate(board)
+        if is_player_turn:
+            best_score = float('-inf')
+            for move in range(NUM_DIRECTIONS):
+                new_board, _, moved = move_board(board, move)
+                if moved:
+                    score = self.expectimax(new_board, depth - 1, False)
+                    best_score = max(best_score, score)
+            return best_score
         else:
-            mini_sample = self.memory
+            empty_cells = [(i, j) for i in range(GRID_SIZE) for j in range(GRID_SIZE) if board[i][j] == 0]
+            if not empty_cells:
+                return self.evaluate(board)
+            total_score = 0.0
+            for i, j in empty_cells:
+                for value, prob in [(2, 0.9), (4, 0.1)]:
+                    new_board = [row[:] for row in board]
+                    new_board[i][j] = value
+                    score = self.expectimax(new_board, depth - 1, True)
+                    total_score += prob * score / len(empty_cells)
+            return total_score
 
-        states, actions, rewards, next_states, dones = zip(*mini_sample)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)
-        
-        # Decay epsilon after each episode
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+    def is_game_over(self, board: List[List[int]]) -> bool:
+        for move in range(NUM_DIRECTIONS):
+            if is_valid_move_board(board, move):
+                return False
+        return True
 
-    def train_short_memory(self, state: np.ndarray, action: List[float], reward: float, 
-                         next_state: np.ndarray, done: bool) -> None:
-        self.trainer.train_step(state, action, reward, next_state, done)
-
-    def get_action(self, state: np.ndarray, game: Game2048 = None, is_testing: bool = False) -> List[float]:
-        # Random moves: tradeoff exploration / exploitation
-        final_move = [0] * 4
-        
-        if is_testing and game is not None:
-            # During testing, only consider valid moves
-            valid_moves = []
-            for move in range(4):
-                if game.is_valid_move(move):
-                    valid_moves.append(move)
-            
-            if not valid_moves:
-                # If no valid moves, return any move (game will end anyway)
-                move = random.randint(0, 3)
-            else:
-                state0 = torch.tensor(state, dtype=torch.float).to(self.device)
-                prediction = self.model(state0)
-                prediction = prediction.detach().cpu().numpy()  # Move back to CPU for numpy operations
-                
-                # Filter predictions to only consider valid moves
-                valid_predictions = [(move, prediction[move]) for move in valid_moves]
-                move = max(valid_predictions, key=lambda x: x[1])[0]
-            
-            final_move[move] = 1
-            return final_move
-            
-        # During training, use epsilon-greedy strategy
-        if random.random() < self.epsilon:
-            move = random.randint(0, 3)
-            final_move[move] = 1
-        else:
-            state0 = torch.tensor(state, dtype=torch.float).to(self.device)
-            prediction = self.model(state0)
-            prediction = prediction.detach().cpu().numpy()  # Move back to CPU for numpy operations
-            move = np.argmax(prediction)
-            final_move[move] = 1
-
-        return final_move 
+    def evaluate(self, board: List[List[int]]) -> int:
+        # Snake pattern para 5x5
+        SNAKE_WEIGHTS: list[list[int]] = [
+            [2**0,  2**1,  2**2,  0,  0],
+            [2**6,  2**5,  2**4,  2**3,  0],
+            [2**7,  2**8,  2**9,  2**10, 2**11],
+            [2**15, 2**14, 2**13, 2**12, 0],
+            [2**16, 2**17, 2**18, 0, 0],
+        ]
+        h: int = 0
+        for i in range(GRID_SIZE):
+            for j in range(GRID_SIZE):
+                h += board[i][j] * SNAKE_WEIGHTS[i][j]
+        return h
--- 2048_AI/test.py
+++ 2048_AI/test.py
@@ -1,53 +1,79 @@
-import torch
 import numpy as np
-from game import Game2048, Direction
-from agent import Agent
-import os
+from game import Game2048
+from agent import ExpectimaxAgent
 import time
+import os
+import csv
+
+MOVE_NAMES = ['MAIN_DIAG_UP', 'SEC_DIAG_UP', 'RIGHT', 'MAIN_DIAG_DOWN', 'SEC_DIAG_DOWN', 'LEFT']
 
 def test(n_games: int = 10) -> None:
-    agent = Agent()
-    game = Game2048()
+    max_depth = 4
+    agent = ExpectimaxAgent(max_depth=max_depth)
     record = 0
     scores = []
+    max_tiles = []
+    game_data = []
 
-    # Load trained model
-    model_path = './model/model.pth'
-    if not os.path.exists(model_path):
-        print("No trained model found. Exiting test.")
-        return
+    print(f"Testando com ExpectimaxAgent (max_depth={max_depth})")
 
-    agent.model.load_state_dict(torch.load(model_path, map_location=agent.device))
-    agent.model.eval()
-    print(f"Loaded model from: {model_path}")
+    # Garante que o diretório de logs existe
+    os.makedirs('logs', exist_ok=True)
+    csv_path = os.path.join('logs', 'test_log.csv')
 
     try:
         for i in range(1, n_games + 1):
-            game.reset()
+            game = Game2048()  # Reinicia o jogo
             state = agent.get_state(game)
-            done = False
+            moves = []
+            move_count = 1
 
-            while not done:
-                final_move = agent.get_action(state, game=game, is_testing=True)
-                reward, done, score = game.play_step(final_move)
+            while not game.game_over:
+                final_move = agent.get_action(state, game=game)
+                move_idx = int(np.argmax(final_move))
+                move_name = MOVE_NAMES[move_idx]
+                moves.append(move_name)
+                move_count += 1
+                moved = game.move(move_idx)
                 state = agent.get_state(game)
+                if not moved:
+                    # Movimento inválido, força término
+                    break
 
             score = game.score
             scores.append(score)
             record = max(record, score)
-            max_tile = int(np.max(game.board))
+            max_tile = int(np.max(game.grid))
+            max_tiles.append(max_tile)
 
-            print(f"Test Game {i} | Score: {score} | Max Tile: {max_tile}")
-            time.sleep(5)
+            # Salva dados da partida
+            game_data.append({
+                'game_number': i,
+                'move_count': move_count - 1,
+                'score': score,
+                'max_tile': max_tile
+            })
+            time.sleep(2)
 
-        avg_score = sum(scores) / len(scores)
-        print(f"\n--- Test Summary ---")
-        print(f"Games Played: {n_games}")
-        print(f"Record Score: {record}")
-        print(f"Average Score: {avg_score:.2f}")
+        max_tile = max(max_tiles)
+        summary = (
+            f"\n--- Test Summary ---\n"
+            f"Games Played: {n_games}\n"
+            f"Record Score: {record}\n"
+            f"Max Tile: {max_tile}\n"
+        )
+        print(summary)
+
+        # Escreve o CSV
+        with open(csv_path, 'w', newline='') as csvfile:
+            fieldnames = ['game_number', 'move_count', 'score', 'max_tile']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in game_data:
+                writer.writerow(row)
 
     except KeyboardInterrupt:
         print("\nTesting interrupted by user.")
 
 if __name__ == '__main__':
-    test(n_games=10)
+    test(n_games=100)
